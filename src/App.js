d26165ea05db955419617a1e9b14bc1e7654ccdb
William Grant <willmgrant@gmail.com>
2019-11-15 21:18:58 +0900

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -8,14 +8,33 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-  return (
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+// change to class for now to use state - later check out react hooks to use state with functional components
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      robots: robots,
+      searchField: ''
+    };
+  }
+
+// use the syntax below when creating custom methods ... Andre will give a resource with more explanation
+  onSearchChange = (event) => {
+    this.setState({ 'searchField': event.target.value });
+  }
+
+  render () {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    });
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const SearchBox = (props) => {
+// using destructuring is using to indicate what variables should be passed to the component
+const SearchBox = ({ searchChange }) => {
   return (
     <div className='pa2'>
       <input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
+        onChange={searchChange}
       />
     </div>
   );
