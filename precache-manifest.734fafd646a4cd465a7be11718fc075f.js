2594ffab32e441ff58fa33ba3ae3e4d33456a189
William Grant <willmgrant@gmail.com>
2019-11-18 18:33:35 +0900

    Updates

---
+++ precache-manifest.734fafd646a4cd465a7be11718fc075f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851607bb8f57af97417721e9fa3a82c7",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "be675809025dd389a408",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1512290148c12d401634",
+    "url": "/robofriends/static/css/main.3f667181.chunk.css"
+  },
+  {
+    "revision": "be675809025dd389a408",
+    "url": "/robofriends/static/js/2.00f7c2b8.chunk.js"
+  },
+  {
+    "revision": "1512290148c12d401634",
+    "url": "/robofriends/static/js/main.6c0a0fc7.chunk.js"
+  },
+  {
+    "revision": "1316333166179fed5736",
+    "url": "/robofriends/static/js/runtime-main.70d17907.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
